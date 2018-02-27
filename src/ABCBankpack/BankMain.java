3ddab017e7fb10636ae2d0a0ed57c3d3319218bd
lidichamp <lidichamp@gmail.com>
2018-02-27 09:37:19 +0100

    ABC BANK

---
+++ src/ABCBankpack/BankMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ABCBankpack;
+
+/**
+ *
+ * @author iyagan
+ */
+public class BankMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home newHome=new Home();
+        newHome.setVisible(true);
+        newHome.setResizable(false);
+        newHome.setLocationRelativeTo(null);
+        
+    }
+    
+}
